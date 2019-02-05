ad39425aac5968e63da8e597d5ae41a9fa944413
Brandon Hass <brandon.jm.hass@gmail.com>
2019-02-05 10:36:03 -0600

    Add files via upload

---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import * as webpackDevServer from 'webpack-dev-server';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration & webpackDevServer.Configuration = {
+  entry: './src/index.tsx',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    progress: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
+
+export default config;
